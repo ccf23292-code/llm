cb6025a3b3ad07b078cae38d2abbdb09ea09f287
ccf23292-code <ccf23292@gmail.com>
2026-06-04 12:18:56 +0800

    乳腺癌二分类：多特征逻辑回归 + 特征缩放 + 训练测试划分

---
+++ cancer_pred.py
@@ -0,0 +1,41 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+import numpy as np
+data = load_breast_cancer()
+X, y = data.data, data.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+mean = X_train.mean(axis = 0)
+std = X_train.std(axis = 0)
+X_train = (X_train - mean) / std
+# 特征值差距过大，会造成梯度膨胀，出现极端情况log0
+# 进行特征缩放
+m = X_train.shape[0]
+y_train = y_train.reshape(-1,1) # y的shape改成（569,1）
+W = np.zeros((30,1),dtype = float)
+b = np.zeros((1,1),dtype = float)
+lr = 1.0
+epochs = 5000
+
+def sigmoid(z):
+    return 1 / (1 + (np.exp(-z)))
+
+for epoch in range(epochs):
+    z = X_train @ W + b
+    y_pred = sigmoid(z)
+    dW = (1/m) * X_train.T @ (y_pred - y_train)
+    db = (1/m) * np.sum(y_pred - y_train,axis = 0, keepdims = True)
+    W -= lr * dW
+    b -= lr * db
+    loss = -np.mean(y_train*np.log(y_pred) + (1-y_train)*np.log(1-y_pred))
+    if ((epoch+1) % 500 == 0):
+        print(f"Epochs: [{epoch+1}/{epochs}] , loss: {loss:.4f}")
+
+pred_train = (sigmoid(X_train @ W + b) >= 0.5)
+accuracy_train = (pred_train == y_train).mean()
+print(f"{accuracy_train = }")
+
+X_test = (X_test - X_test.mean(axis = 0)) / X_test.std(axis = 0)
+pred_test = (sigmoid(X_test @ W + b) >= 0.5)
+y_test = y_test.reshape(-1,1)
+accuracy_test = (pred_test == y_test).mean()   
+print(f"{accuracy_test = }")
